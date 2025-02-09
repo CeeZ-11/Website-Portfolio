46b15a32fc6fcd38f4a3b777e7ccd71ff3f99cf0
Seamor Estrabon <seamorestrabon@gmail.com>
2025-02-10 06:11:30 +0800

    :sparkles: feat: Add profile card in aside component with image, name, role and also layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,18 @@
 import "../styles/MainLayout.css";
+import profileImg from "../assets/profile.webp";
 
 export default function MainLayout() {
   return (
     <div className="Main">
       <aside>
-        <h1>Aside</h1>
+        <div className="profile-card">
+          <img src={profileImg} alt="profile" />
+          <div className="profile-name">
+            <h2>Seamor Estrabon</h2>
+            <hr className="short-line" />
+            <h4>Front-End Developer</h4>
+          </div>
+        </div>
       </aside>
       <main>
         <h1>Main</h1>
