df05e887e720d8bc1790d6fc3d5d3a3c1f2d5051
Seamor Estrabon <seamorestrabon@gmail.com>
2025-02-10 07:12:41 +0800

    :sparkles: feat: Add profile details with icons

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,8 @@
 import "../styles/MainLayout.css";
 import profileImg from "../assets/profile.webp";
+import emailIcon from "../assets/email.svg";
+import phoneIcon from "../assets/phone.svg";
+import addressIcon from "../assets/address.svg";
 
 export default function MainLayout() {
   return (
@@ -11,6 +14,30 @@ export default function MainLayout() {
             <h2>Seamor Estrabon</h2>
             <hr className="short-line" />
             <h4>Front-End Developer</h4>
+          </div>
+        </div>
+        <hr className="divider-line" />
+        <div className="profile-details">
+          <div className="email">
+            <img src={emailIcon} alt="email" />
+            <div className="email-details">
+              <h5>Email: </h5>
+              <p>seamorestrabon@gmail.com</p>
+            </div>
+          </div>
+          <div className="phone">
+            <img src={phoneIcon} alt="phone" />
+            <div className="phone-details">
+              <h5>Phone: </h5>
+              <p>+639270664116</p>
+            </div>
+          </div>
+          <div className="address">
+            <img src={addressIcon} alt="address" />
+            <div className="address-details">
+              <h5>Address: </h5>
+              <p>Bacolod City, Philippines</p>
+            </div>
           </div>
         </div>
       </aside>
