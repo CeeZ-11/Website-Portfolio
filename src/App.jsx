21e68ad2f3caa3fd95cc54c87c6032ff4492b111
Seamor Estrabon <seamorestrabon@gmail.com>
2025-02-10 13:44:22 +0800

    Add package for user event in testing library for Vitest

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
 import MainLayout from "./layouts/MainLayout";
+import { useState } from "react";
 
 function App() {
+  const [heading, setHeading] = useState("Magnificent Monkeys");
+
+  const clickHandler = () => {
+    setHeading("Radical Rhinos");
+  };
+
   return (
     <div className="App">
       <MainLayout />
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
     </div>
   );
 }
