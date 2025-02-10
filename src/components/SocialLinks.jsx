7bd02f253e7be542420750495ae2ed45f812d2af
Seamor Estrabon <seamorestrabon@gmail.com>
2025-02-10 12:07:30 +0800

    Update link target to open in a new tab

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -2,7 +2,12 @@ export default function SocialLinks({ links }) {
   return (
     <div className="profile-socials">
       {links.map((link) => (
-        <a key={link.name} href={link.href}>
+        <a
+          key={link.name}
+          href={link.href}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={link.icon} alt={link.name} loading="lazy" />
         </a>
       ))}
