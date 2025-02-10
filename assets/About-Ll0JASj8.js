05430fb8df9d352de75e2320243e87054004df70
CeeZ-11 <CeeZ-11@users.noreply.github.com>
2025-02-10 06:30:12 +0000

    deploy: 50e29770c7cd55935080c4beb26ac747fb29354b

---
+++ assets/About-Ll0JASj8.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DwZvM2hn.js";function r(){return t.jsx("h1",{children:"About Page"})}export{r as default};
---
+++ assets/Portfolio-CBC7XLBu.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DwZvM2hn.js";function t(){return o.jsx("h1",{children:"Portfolio Page"})}export{t as default};
