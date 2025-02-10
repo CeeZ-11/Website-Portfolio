86d374e956358baccbf71c8f97251f0035c4c902
Seamor Estrabon <seamorestrabon@gmail.com>
2025-02-10 08:18:02 +0800

    Install vite-plugin-compression to enable gzip compression for optimized asset delivery

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import compression from "vite-plugin-compression";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
@@ -8,6 +9,6 @@ export default defineConfig({
       usePolling: true,
     },
   },
-  plugins: [react()],
+  plugins: [react(), compression()],
   base: "/Website-Portfolio/",
 });
