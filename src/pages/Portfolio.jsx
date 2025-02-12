f8605a5f55d0c7e28055ba218488a5807f9d9550
Seamor Estrabon <seamorestrabon@gmail.com>
2025-02-13 06:11:44 +0800

    :sparkles: feat: Add nav buttons and reuse NavButton component to Portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,59 @@
 import PageTitle from "../components/PageTitle";
+import NavButton from "../components/NavButton";
+import { useState } from "react";
 
 export default function Portfolio() {
+  const [activeTab, setActiveTab] = useState("All Projects");
+
+  const navButtons = [
+    { id: "all-projects", name: "All Projects" },
+    { id: "odin-projects", name: "Odin Projects" },
+    { id: "mini-projects", name: "Mini Projects" },
+    { id: "personal-projects", name: "Personal Projects" },
+    { id: "collaborations", name: "Collaborations" },
+  ];
+
   return (
     <>
       <PageTitle name="Portfolio" />
+      <div className="project-container">
+        <nav className="project-nav">
+          <ul>
+            <NavButton
+              navButtons={navButtons}
+              activeTab={activeTab}
+              setActiveTab={setActiveTab}
+            />
+          </ul>
+        </nav>
+        <div className="project-content">
+          {activeTab === "all-projects" && (
+            <>
+              <h4> All Projects </h4>
+            </>
+          )}
+          {activeTab === "odin-projects" && (
+            <>
+              <h4> Odin Projects </h4>
+            </>
+          )}
+          {activeTab === "mini-projects" && (
+            <>
+              <h4> Mini Projects </h4>
+            </>
+          )}
+          {activeTab === "personal-projects" && (
+            <>
+              <h4> Personal Projects </h4>
+            </>
+          )}
+          {activeTab === "collaborations" && (
+            <>
+              <h4> Collaborations </h4>
+            </>
+          )}
+        </div>
+      </div>
     </>
   );
 }
