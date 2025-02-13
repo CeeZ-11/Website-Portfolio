62a9c81143748702d26f19f7703950d18fd42f2e
CeeZ-11 <CeeZ-11@users.noreply.github.com>
2025-02-13 00:47:22 +0000

    deploy: 5acf19c723857b4836ebff02f2bfe7f0877546d7

---
+++ assets/PageTitle-BAF2oC8q.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BMf-3NzX.js";function t({name:s}){return e.jsxs("div",{className:"about-title",children:[e.jsx("h1",{children:s}),e.jsx("hr",{className:"header-line"})]})}export{t as P};
