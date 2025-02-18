ca7c44cf0afee81a2364e1168f352258b4073795
CeeZ-11 <CeeZ-11@users.noreply.github.com>
2025-02-18 21:51:21 +0000

    deploy: e8aa84283fb5d0e366d80b32c5916156eb7bfd5f

---
+++ assets/PageTitle-CIaykGhT.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-AyHUpXOK.js";function t({name:s}){return e.jsxs("div",{className:"about-title",children:[e.jsx("h1",{children:s}),e.jsx("hr",{className:"header-line"})]})}export{t as P};
