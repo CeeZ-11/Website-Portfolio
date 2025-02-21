17f54e2bbbd3411922425ca662b0ffe8ab9caaa8
Seamor Estrabon <seamorestrabon@gmail.com>
2025-02-21 21:16:18 +0800

    :sparkles: feat: Add critters for css rendering block

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import compression from "vite-plugin-compression";
 import react from "@vitejs/plugin-react";
+import Critters from "critters";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,7 +10,36 @@ export default defineConfig({
       usePolling: true,
     },
   },
-  plugins: [react(), compression()],
+  plugins: [
+    react(),
+    compression(),
+    {
+      name: "vite-plugin-critters",
+      apply: "build",
+      enforce: "post",
+      async closeBundle() {
+        const { readFile, writeFile } = await import("fs/promises");
+        const path = "./dist/index.html";
+        try {
+          let html = await readFile(path, "utf8");
+          const critters = new Critters({
+            preload: "swap",
+            inlineFonts: true,
+            pruneSource: true,
+          });
+          html = await critters.process(html);
+          await writeFile(path, html, "utf8");
+          console.log("✅ Critters applied: Critical CSS inlined!");
+        } catch (error) {
+          console.error("❌ Critters processing failed:", error);
+        }
+      },
+    },
+  ],
+  build: {
+    cssCodeSplit: true,
+    outDir: "dist",
+  },
   base: "/Website-Portfolio/",
   test: {
     globals: true,
