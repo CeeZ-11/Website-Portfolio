ce5659cebd02a92b5df4f827151ef7dd55ae9339
Seamor Estrabon <seamorestrabon@gmail.com>
2025-02-23 22:33:59 +0800

    :sparkles: feat: Add shopping cart project to portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -18,6 +18,7 @@ import signUpFormScreenshot from "../assets/signup-form.webp";
 import calculatorScreenshot from "../assets/calculator.webp";
 import etchASketchScreenshot from "../assets/etch-a-sketch.webp";
 import landingPageScreenshot from "../assets/landing-page.webp";
+import shoppingCartScreenshot from "../assets/shopping-cart.webp";
 
 export default function Portfolio() {
   const [isPortVisible, setPortIsVisible] = useState(false);
@@ -34,104 +35,111 @@ export default function Portfolio() {
   const projectList = [
     {
       id: 1,
+      name: "Shopping Cart",
+      image: shoppingCartScreenshot,
+      category: "odin-projects",
+      url: "https://ceez-11.github.io/Odin-Project-Shopping-Cart/",
+    },
+    {
+      id: 2,
       name: "Memory Card",
       image: memoryCardScreenshot,
       category: "odin-projects",
       url: "https://ceez-11.github.io/Odin-Project-Memory-Card/",
     },
     {
-      id: 2,
+      id: 3,
       name: "CV Application",
       image: cvApplicationScreenshot,
       category: "odin-projects",
       url: "https://ceez-11.github.io/Odin-Project-CV-Application/",
     },
     {
-      id: 3,
+      id: 4,
       name: "Weather App",
       image: weatherAppScreenshot,
       category: "odin-projects",
       url: "https://ceez-11.github.io/Odin-Project-Weather-App/",
     },
     {
-      id: 4,
+      id: 5,
       name: "Pokemon Guessing Game",
       image: pokemonGuessingGameScreenshot,
       category: "mini-projects",
       url: "https://ceez-11.github.io/Pokemon-Guessing-Game/",
     },
     {
-      id: 5,
+      id: 6,
       name: "Battleship Game",
       image: battleShipScreenshot,
       category: "odin-projects",
       url: "https://ceez-11.github.io/Odin-Project-Battleship/",
     },
     {
-      id: 6,
+      id: 7,
       name: "Rock Paper Scissors Game",
       image: rockPaperScissorsScreenshot,
       category: "odin-projects",
       url: "https://ceez-11.github.io/Odin-Project-Rock-Paper-Scissors/",
     },
     {
-      id: 7,
+      id: 8,
       name: "Library App",
       image: libraryScreenshot,
       category: "odin-projects",
       url: "https://ceez-11.github.io/Odin-Project-Library/",
     },
     {
-      id: 8,
+      id: 9,
       name: "Todo List App",
       image: todoListScreenshot,
       category: "odin-projects",
       url: "https://ceez-11.github.io/Odin-Project-TodoList/",
     },
     {
-      id: 9,
+      id: 10,
       name: "Restaurant Page",
       image: restaurantPage,
       category: "odin-projects",
       url: "https://ceez-11.github.io/Odin-Project-Restaurant-Page/",
     },
     {
-      id: 10,
+      id: 11,
       name: "Tic-Tac-Toe Game",
       image: ticTacToeScreenshot,
       category: "odin-projects",
       url: "https://ceez-11.github.io/Odin-Project-Tic-Tac-Toe/",
     },
     {
-      id: 11,
+      id: 12,
       name: "Admin Dashboard",
       image: adminDashboardScreenshot,
       category: "odin-projects",
       url: "https://ceez-11.github.io/Odin-Project-Admin-Dashboard/",
     },
     {
-      id: 12,
+      id: 13,
       name: "Sign Up Form",
       image: signUpFormScreenshot,
       category: "odin-projects",
       url: "https://ceez-11.github.io/Odin-Project-Sign-up-Form/",
     },
     {
-      id: 13,
+      id: 14,
       name: "Calculator App",
       image: calculatorScreenshot,
       category: "odin-projects",
       url: "https://ceez-11.github.io/Odin-Project-Calculator/",
     },
     {
-      id: 14,
+      id: 15,
       name: "Etch a Sketch",
       image: etchASketchScreenshot,
       category: "odin-projects",
       url: "https://ceez-11.github.io/Odin-Project-Etch-a-Sketch/",
     },
     {
-      id: 15,
+      id: 16,
       name: "Landing Page",
       image: landingPageScreenshot,
       category: "odin-projects",
