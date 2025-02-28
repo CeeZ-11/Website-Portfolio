4c9235a492919cf9a10c621759ae8a96dd6a0889
Seamor Estrabon <seamorestrabon@gmail.com>
2025-02-28 17:10:50 +0800

    :wrench: chore: refactor Project component and create a projectItem component
    :wrench: chore: Add useOnScreen hook in ProjectItem component
    
    :art: style: update fade in transform translateY and speed of transition

---
+++ src/components/ProjectItem.jsx
@@ -0,0 +1,35 @@
+import useOnScreen from "../hooks/useOnScreen";
+import OpenLinkIcon from "../components/Icons/OpenLinkIcon";
+
+const ProjectItem = ({ project }) => {
+  const [ref, isVisible] = useOnScreen();
+
+  return (
+    <li ref={ref} className={`fade-in-section ${isVisible ? "visible" : ""}`}>
+      <a
+        href={project.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ textDecoration: "none", color: "inherit" }}
+      >
+        <div className="project-image">
+          <img
+            src={project.image}
+            srcSet={project.image}
+            alt={project.name}
+            loading="lazy"
+          />
+          <div className="visit-site-icon">
+            <OpenLinkIcon />
+          </div>
+        </div>
+      </a>
+      <div className="project-details">
+        <h5>{project.name}</h5>
+        <h3>{project.category}</h3>
+      </div>
+    </li>
+  );
+};
+
+export default ProjectItem;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import OpenLinkIcon from "../components/Icons/OpenLinkIcon";
+import ProjectItem from "./ProjectItem";
 
 export default function Projects({ projectList }) {
   if (projectList.length === 0) {
@@ -11,34 +11,9 @@ export default function Projects({ projectList }) {
 
   return (
     <ul>
-      {projectList.map((project) => {
-        return (
-          <li key={project.id}>
-            <a
-              href={project.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ textDecoration: "none", color: "inherit" }}
-            >
-              <div className="project-image">
-                <img
-                  src={project.image}
-                  srcSet={project.image}
-                  alt={project.name}
-                  loading="lazy"
-                />
-                <div className="visit-site-icon">
-                  <OpenLinkIcon />
-                </div>
-              </div>
-            </a>
-            <div className="project-details">
-              <h5>{project.name}</h5>
-              <h3>{project.category}</h3>
-            </div>
-          </li>
-        );
-      })}
+      {projectList.map((project) => (
+        <ProjectItem key={project.id} project={project} />
+      ))}
     </ul>
   );
 }
